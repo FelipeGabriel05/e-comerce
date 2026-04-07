ed8af5b21f3320502041162e5ee1e75f6dcc0df3
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-04-07 01:19:27 -0300

    ♻️ initialize the `shadcn`

--- client/src/lib/pages/home/index.tsx
+++ client/src/lib/pages/home/index.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 import { CTASection } from './components/cta-section';
 
 const Home = () => {
   return (
     <div className="flex min-h-[60vh] flex-col items-center justify-center gap-8 text-center">
       <CTASection />
+      <Button> This is a button </Button>
     </div>
   );
 };
