d58f397595588596ec6225ceb4f08b8ad193c4b8
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-04-10 09:53:52 -0300

    ✨ add `POST /admin/products` endpoint

---
+++ api/src/main/java/ecommerce/Http/Controller/ProductController.java
@@ -0,0 +1,21 @@
+package ecommerce.Http.Controller;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/products")
+public class ProductController extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  public ProductController() {
+    super();
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("application/json");
+  }
+}
