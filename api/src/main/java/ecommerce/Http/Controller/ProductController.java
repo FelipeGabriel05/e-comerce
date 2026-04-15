d13a7a950622b1bf717ebd8413410c638e1a7b81
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-04-15 16:56:04 -0300

    ♻️ config `POST /admin/products` to accept multipart form

--- api/src/main/java/ecommerce/Http/Controller/ProductController.java
+++ api/src/main/java/ecommerce/Http/Controller/ProductController.java
@@ -6,12 +6,14 @@ import ecommerce.Http.Validators.HttpProductValidators;
 import ecommerce.UseCases.CreateProductUseCase;
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/admin/products")
+@MultipartConfig
 public class ProductController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
