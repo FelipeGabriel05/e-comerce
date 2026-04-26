fa686d58c912ea7186bff4db2d5cade71f751ff2
FelipeGabriel05 <felpsgb2321@gmail.com>
2026-04-26 13:37:07 -0300

    modifications knip

--- client/knip.ts
+++ client/knip.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
 	entry: ['src/main.tsx'],
-	ignore: ['src/**/*.gen.ts'],
+	ignore: ['src/**/*.gen.ts', 'src/components/ui/**'],
 	project: ['src/**/*.{ts,tsx,js,jsx,css,scss}'],
 	ignoreBinaries: ['changelogithub'],
 	ignoreDependencies: ["lucide-react"]
