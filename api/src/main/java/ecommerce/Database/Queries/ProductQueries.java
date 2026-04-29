c9b99a5af08f0ab8796a1a6caa6e3ee1f4452440
DerikTech <madrinckjr@gmail.com>
2026-04-29 15:29:07 -0300

    Update ProductQueries.java
    Querie do Lista de Produtos foi adicionado

--- api/src/main/java/ecommerce/Database/Queries/ProductQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/ProductQueries.java
@@ -3,4 +3,8 @@ package ecommerce.Database.Queries;
 public class ProductQueries {
   public static String insertProductQuery =
       "INSERT INTO produto (descricao, preco, foto, quantidade, categoria_id) VALUES (?, ?, ?, ?, ?)";
+
+  public static String listAvailableProductsQuery =
+      "SELECT id, descricao, preco, foto, quantidade, categoria_id " +
+      "FROM produto WHERE quantidade > 0";
 }
