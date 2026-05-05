b42cb9163d02be0048bab53fb9a561315c4a5404
DerikTech <madrinckjr@gmail.com>
2026-05-04 22:56:33 -0300

    Update UpdateUseCase.java
    mesmo dos anterior

--- api/src/main/java/ecommerce/UseCases/UpdateUseCase.java
+++ api/src/main/java/ecommerce/UseCases/UpdateUseCase.java
@@ -1,19 +1,24 @@
 package ecommerce.UseCases;
 
+import ecommerce.Database.DBConnection;
 import ecommerce.Database.Entites.User;
 import ecommerce.Database.Repositories.UserRepository;
 import ecommerce.Exceptions.ValidationException;
+import java.sql.Connection;
 
-public class UpdateUserUseCase {
-
-  private UserRepository userRepository = new UserRepository();
-
+public class UpdateUseCase {
   public void execute(User user) throws ValidationException {
+    try {
+      Connection con = DBConnection.getConnection();
+      UserRepository userRepository = new UserRepository(con);
+      boolean updated = userRepository.updateUser(user);
 
-    boolean updated = userRepository.updateUser(user);
-
-    if (!updated) {
-      throw new ValidationException("User not found or could not be updated");
+      if (!updated) {
+        throw new ValidationException("User not found or could not be updated");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new ValidationException("Internal error while updating user");
     }
   }
 }
