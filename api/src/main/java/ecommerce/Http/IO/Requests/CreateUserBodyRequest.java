69d651720cc26a97d05ea0e4c273e7f75c0e63a3
pvcito <112362159+pvcito@users.noreply.github.com>
2026-05-10 22:01:43 +0000

    Add CreateUserBodyRequest DTO

---
+++ api/src/main/java/ecommerce/Http/IO/Requests/CreateUserBodyRequest.java
@@ -0,0 +1,9 @@
+package ecommerce.Http.IO.Requests;
+
+public class CreateUserBodyRequest {
+  public String name;
+  public String address;
+  public String email;
+  public String login;
+  public String password;
+}
