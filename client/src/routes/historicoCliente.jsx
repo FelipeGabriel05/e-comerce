b9cb325e072b434e8c7ab4f3459213e9592aa52c
FelipeGabriel05 <felpsgb2321@gmail.com>
2026-05-09 21:46:28 -0300

    layout construction

--- client/src/routes/historicoCliente.jsx
+++ client/src/routes/historicoCliente.jsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import historicoPedidos from '@/lib/pages/clients/historicoPedidos';
+import historicoPedidos from '@/lib/pages/clients/historico-pedidos';
 
 export const Route = createFileRoute('/historicoCliente')({
   component: historicoPedidos,
