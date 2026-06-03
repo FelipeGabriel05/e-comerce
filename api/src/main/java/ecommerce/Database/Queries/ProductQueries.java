76ef62f7df54d77943f755df1d0249482f445e22
DerikTech <madrinckjr@gmail.com>
2026-06-03 16:52:26 +0000

    🚧 Querie do UpdateProduct temporario

--- api/src/main/java/ecommerce/Database/Queries/ProductQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/ProductQueries.java
@@ -4,4 +4,8 @@ public class ProductQueries {
   public static String insertProductQuery =
       "INSERT INTO produto (descricao, preco, foto, quantidade, categoria_id) VALUES (?, ?, ?, ?, ?)";
   public static String selectProductByIdQuery = "SELECT * FROM produto WHERE id = ?";
+
+  public static String updateProductQuery = 
+  "UPDATE produto SET descricao = ?, preco = ?, foto = ?, quantidade = ?, categoria_id = ?, WHERE id =?";
 }
+
