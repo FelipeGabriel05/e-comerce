1ce9fa734b52914d85dde43067a413729b137c9d
Joao Victor <joaoaprigio10303@gmail.com>
2026-06-06 10:32:12 -0300

    fix: corrige formatação dos controllers Java

--- api/src/main/java/ecommerce/Http/Controller/UserController.java
+++ api/src/main/java/ecommerce/Http/Controller/UserController.java
@@ -23,53 +23,34 @@ public class UserController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   private HttpUserValidators validator = new HttpUserValidators();
-
   private DeleteUserUseCase deleteUseCase = new DeleteUserUseCase();
-
   private UpdateUseCase updateUseCase = new UpdateUseCase();
 
   @Override
   protected void doPut(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
     response.setContentType("application/json");
-
     try {
       int id = PathVariableExtractor.extractIntPathVariable(request, "/users/:id", "id");
-
       User user = validator.validateUpdateUser(request, id);
-
       updateUseCase.execute(user);
-
       user.setSenha(null);
-
       JsonResponse jsonRes =
           new JsonResponse(HttpServletResponse.SC_OK, "User Updated sucessfully", user);
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
-
     } catch (ValidationException e) {
-
       JsonResponse jsonRes = new JsonResponse(422, e.getMessage());
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
-
     } catch (NotFoundException e) {
-
       JsonResponse jsonRes = new JsonResponse(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
-
     } catch (Exception e) {
-
       e.printStackTrace();
-
       JsonResponse jsonRes =
           new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
     }
@@ -78,33 +59,22 @@ public class UserController extends HttpServlet {
   @Override
   protected void doDelete(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
     response.setContentType("application/json");
-
     try {
       int id = PathVariableExtractor.extractIntPathVariable(request, "/users/:id", "id");
-
       deleteUseCase.execute(id);
-
       JsonResponse jsonRes =
           new JsonResponse(HttpServletResponse.SC_OK, "User Deleted sucessfully", id);
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
-
     } catch (ValidationException e) {
-
       JsonResponse jsonRes = new JsonResponse(422, e.getMessage());
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
-
     } catch (Exception e) {
       e.printStackTrace();
-
       JsonResponse jsonRes =
           new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
     }
