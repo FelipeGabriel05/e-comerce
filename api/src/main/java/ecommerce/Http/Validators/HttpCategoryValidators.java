f0670424494768672f899e27b735d259281e1f14
Paulo Victor <112362159+pv-lima@users.noreply.github.com>
2026-06-15 19:57:42 +0000

    feat: add validateUpdateCategory method to validator

--- api/src/main/java/ecommerce/Http/Validators/HttpCategoryValidators.java
+++ api/src/main/java/ecommerce/Http/Validators/HttpCategoryValidators.java
@@ -4,6 +4,7 @@ import ecommerce.Database.Entites.Category;
 import ecommerce.Exceptions.ValidationException;
 import ecommerce.Http.IO.BodyJsonToObject;
 import ecommerce.Http.IO.Requests.CreateCategoryBodyRequest;
+import ecommerce.Http.IO.Requests.UpdateCategoryBodyRequest;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -41,6 +42,45 @@ public class HttpCategoryValidators {
     }
 
     Category category = new Category();
+    category.setDescricao(body.descricao);
+
+    return category;
+  }
+
+  public Category validateUpdateCategory(HttpServletRequest request) throws ValidationException {
+
+    List<String> errors = new ArrayList<>();
+
+    UpdateCategoryBodyRequest body = null;
+
+    try {
+      body = BodyJsonToObject.parse(request, UpdateCategoryBodyRequest.class);
+    } catch (Exception e) {
+      throw new ValidationException("Invalid body data");
+    }
+
+    if (body.id <= 0) {
+      errors.add("Valid ID is required");
+    }
+
+    if (body.descricao == null || body.descricao.isBlank()) {
+      errors.add("Description is required");
+    } else {
+      if (body.descricao.length() < 3) {
+        errors.add("Description must contain at least 3 characters");
+      }
+      if (body.descricao.length() > 255) {
+        errors.add("Description must contain at most 255 characters");
+      }
+    }
+
+    String errorMessage = String.join(", ", errors);
+    if (!errorMessage.isEmpty()) {
+      throw new ValidationException(errorMessage);
+    }
+
+    Category category = new Category();
+    category.setId(body.id);
     category.setDescricao(body.descricao);
 
     return category;
