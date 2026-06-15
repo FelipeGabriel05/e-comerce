f20da36b1ad45bd0d3cbb5482e0097206db4595d
Paulo Victor <112362159+pv-lima@users.noreply.github.com>
2026-06-15 19:05:44 +0000

    feat: create ListCategoriesUseCase for fetching categories

---
+++ api/src/main/java/ecommerce/UseCases/ListCategoriesUseCase.java
@@ -0,0 +1,25 @@
+package ecommerce.UseCases;
+
+import ecommerce.Database.DBConnection;
+import ecommerce.Database.Entites.Category;
+import ecommerce.Database.Repositories.CategoriesRepository;
+import java.sql.Connection;
+import java.util.List;
+
+public class ListCategoriesUseCase {
+    
+    public ListCategoriesUseCase(){}
+        
+    public List<Category> execute() throws Exception {
+        try {
+            Connection dbConnection = DBConnection.getConnection();
+            CategoriesRepository categoriesRepository = new CategoriesRepository(dbConnection);
+
+            return categoriesRepository.listCategories();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
