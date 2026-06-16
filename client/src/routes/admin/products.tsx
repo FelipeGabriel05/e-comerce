595a4755e008889b65075f4eed7e79192f372b1c
Joao Victor <joaoaprigio10303@gmail.com>
2026-06-16 20:42:58 -0300

    refactor: move página de produtos admin para rota /admin/produtos

--- client/src/routes/admin/products.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import AdminProducts from '@/lib/pages/admin/products';
-
-export const Route = createFileRoute('/admin/products')({
-  component: AdminProducts,
-});
