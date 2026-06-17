826c87285f3f48d9b374ccf2b7d9635637ba9d9a
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-06-17 15:23:06 -0300

    🩹 correcting the conflict resolution that Derik handled poorly

--- api/src/main/java/ecommerce/Http/Controller/AdminProductController.java
+++ api/src/main/java/ecommerce/Http/Controller/AdminProductController.java
@@ -1,9 +1,13 @@
 package ecommerce.Http.Controller;
 
 import ecommerce.Database.Entites.Product;
+import ecommerce.Exceptions.NotFoundException;
+import ecommerce.Exceptions.ValidationException;
+import ecommerce.Http.IO.PathVariableExtractor;
 import ecommerce.Http.IO.Responses.JsonResponse;
 import ecommerce.Http.Validators.HttpProductValidators;
 import ecommerce.UseCases.CreateProductUseCase;
+import ecommerce.UseCases.UpdateProductUseCase;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -12,7 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/products")
+@WebServlet("/admin/products/*")
 @MultipartConfig
 public class AdminProductController extends HttpServlet {
 
@@ -49,6 +53,58 @@ public class AdminProductController extends HttpServlet {
       e.printStackTrace();
 
       JsonResponse jsonRes = new JsonResponse(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+
+      response.setStatus(jsonRes.getStatus());
+      response.getWriter().write(jsonRes.toJson());
+    }
+  }
+
+  @Override
+  protected void doPut(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    HttpProductValidators validators = new HttpProductValidators();
+
+    response.setContentType("application/json");
+
+    try {
+      int id = PathVariableExtractor.extractIntPathVariable(request, "/admin/products/:id", "id");
+
+      Product productInput = validators.validateUpdateProduct(request, id);
+
+      UpdateProductUseCase useCase = new UpdateProductUseCase();
+
+      Product updatedProduct = useCase.execute(productInput);
+
+      if (updatedProduct != null) {
+
+        JsonResponse jsonRes =
+            new JsonResponse(HttpServletResponse.SC_OK, "Product updated", updatedProduct);
+
+        response.setStatus(jsonRes.getStatus());
+        response.getWriter().write(jsonRes.toJson());
+      }
+
+    } catch (ValidationException e) {
+
+      JsonResponse jsonRes = new JsonResponse(422, e.getMessage());
+
+      response.setStatus(jsonRes.getStatus());
+      response.getWriter().write(jsonRes.toJson());
+
+    } catch (NotFoundException e) {
+
+      JsonResponse jsonRes = new JsonResponse(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
+
+      response.setStatus(jsonRes.getStatus());
+      response.getWriter().write(jsonRes.toJson());
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+
+      JsonResponse jsonRes =
+          new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
 
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
--- api/src/main/java/ecommerce/Http/Controller/ProductController.java
+++ api/src/main/java/ecommerce/Http/Controller/ProductController.java
@@ -1,23 +1,16 @@
 package ecommerce.Http.Controller;
 
 import ecommerce.Database.Entites.Product;
-import ecommerce.Exceptions.NotFoundException;
-import ecommerce.Exceptions.ValidationException;
-import ecommerce.Http.IO.PathVariableExtractor;
 import ecommerce.Http.IO.Responses.JsonResponse;
-import ecommerce.Http.Validators.HttpProductValidators;
 import ecommerce.UseCases.ListProductsUseCase;
-import ecommerce.UseCases.UpdateProductUseCase;
 import java.io.IOException;
 import java.util.List;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/products/*")
-@MultipartConfig
+@WebServlet("/products")
 public class ProductController extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
@@ -43,58 +36,6 @@ public class ProductController extends HttpServlet {
       e.printStackTrace();
 
       JsonResponse jsonRes = new JsonResponse(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
-
-      response.setStatus(jsonRes.getStatus());
-      response.getWriter().write(jsonRes.toJson());
-    }
-  }
-
-  @Override
-  protected void doPut(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-
-    HttpProductValidators validators = new HttpProductValidators();
-
-    response.setContentType("application/json");
-
-    try {
-      int id = PathVariableExtractor.extractIntPathVariable(request, "/admin/products/:id", "id");
-
-      Product productInput = validators.validateUpdateProduct(request, id);
-
-      UpdateProductUseCase useCase = new UpdateProductUseCase();
-
-      Product updatedProduct = useCase.execute(productInput);
-
-      if (updatedProduct != null) {
-
-        JsonResponse jsonRes =
-            new JsonResponse(HttpServletResponse.SC_OK, "Product updated", updatedProduct);
-
-        response.setStatus(jsonRes.getStatus());
-        response.getWriter().write(jsonRes.toJson());
-      }
-
-    } catch (ValidationException e) {
-
-      JsonResponse jsonRes = new JsonResponse(422, e.getMessage());
-
-      response.setStatus(jsonRes.getStatus());
-      response.getWriter().write(jsonRes.toJson());
-
-    } catch (NotFoundException e) {
-
-      JsonResponse jsonRes = new JsonResponse(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
-
-      response.setStatus(jsonRes.getStatus());
-      response.getWriter().write(jsonRes.toJson());
-
-    } catch (Exception e) {
-
-      e.printStackTrace();
-
-      JsonResponse jsonRes =
-          new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
 
       response.setStatus(jsonRes.getStatus());
       response.getWriter().write(jsonRes.toJson());
