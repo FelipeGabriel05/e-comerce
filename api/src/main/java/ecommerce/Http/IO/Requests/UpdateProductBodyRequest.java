cad8fab9714a44f285c04b39ca71e5a206361140
DerikTech <madrinckjr@gmail.com>
2026-06-17 02:09:34 +0000

    ✨ Criação do bodyrequest proprio ao codigo

---
+++ api/src/main/java/ecommerce/Http/IO/Requests/UpdateProductBodyRequest.java
@@ -0,0 +1,10 @@
+package ecommerce.Http.IO.Requests;
+
+public class UpdateProductBodyRequest {
+    public int id;
+    public String descricao;
+    public double preco;
+    public String foto;
+    public int quantidade;
+    public int categoriaId;
+}
