165eacfedda80f1cc9b2de34578b2907a319187a
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-06-18 14:11:57 -0300

    🐛 invalid query for updating product

--- api/src/main/java/ecommerce/Database/Queries/ProductQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/ProductQueries.java
@@ -6,9 +6,10 @@ public class ProductQueries {
 
   public static String listAvailableProductsQuery =
       "SELECT id, descricao, preco, foto, quantidade, categoria_id "
-          + "FROM produto WHERE quantidade > 0";
+          + "FROM produto WHERE quantidade > 0 ORDER BY id ASC";
+
   public static String selectProductByIdQuery = "SELECT * FROM produto WHERE id = ?";
 
   public static String updateProductQuery =
-      "UPDATE produto SET descricao = ?, preco = ?, foto = ?, quantidade = ?, categoria_id = ?, WHERE id =?";
+      "UPDATE produto SET descricao = ?, preco = ?, foto = ?, quantidade = ?, categoria_id = ? WHERE id =?";
 }
