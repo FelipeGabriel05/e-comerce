9dda67fc1381ffd85ac704b1aff981c2262dc700
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-06-18 14:12:51 -0300

    🦺 add validations for `foto`

--- api/src/main/java/ecommerce/Http/Validators/HttpProductValidators.java
+++ api/src/main/java/ecommerce/Http/Validators/HttpProductValidators.java
@@ -37,6 +37,10 @@ public class HttpProductValidators {
       errors.add("Category ID is required");
     }
 
+    if (body.foto != null && !body.foto.isEmpty() && !body.foto.contains(";base64,")) {
+      errors.add("Photo must be a valid image");
+    }
+
     String errorMessage = String.join(", ", errors);
     if (!errorMessage.isEmpty()) {
       throw new ValidationException(errorMessage);
@@ -84,6 +88,13 @@ public class HttpProductValidators {
 
     if (body.categoriaId <= 0) {
       errors.add("Category ID is required");
+    }
+
+    if (body.foto != null
+        && !body.foto.isEmpty()
+        && !body.foto.startsWith("http")
+        && !body.foto.contains(";base64,")) {
+      errors.add("Photo must be a URL or a valid image");
     }
 
     if (!errors.isEmpty()) {
