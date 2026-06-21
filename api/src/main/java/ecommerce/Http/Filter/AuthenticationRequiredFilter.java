a11c910942764e32669cee12c62a965198ee6918
DerikTech <madrinckjr@gmail.com>
2026-06-21 18:26:44 +0000

    🔒 Atualizei rotas protegidas por autenticação

--- api/src/main/java/ecommerce/Http/Filter/AuthenticationRequiredFilter.java
+++ api/src/main/java/ecommerce/Http/Filter/AuthenticationRequiredFilter.java
@@ -35,6 +35,7 @@ public class AuthenticationRequiredFilter implements Filter {
           new Route("DELETE", "/admin/products/*"),
           new Route("PUT", "/admin/category/*"),
           new Route("PUT", "/admin/products/*"),
+          new Route("POST", "/checkout"),
           new Route("POST", "/logout"));
 
   @Override
