dfa68695015845ae19b0944a00442ffb5f1fdebf
DerikTech <madrinckjr@gmail.com>
2026-06-21 00:07:07 +0000

    🎨 Adições para usos necessarios de estoque

--- api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
@@ -5,4 +5,11 @@ public class SaleQueries {
 
   public static String insertSaleItemQuery =
       "INSERT INTO venda_produto (venda_id, produto_id, preco, quantidade) VALUES (?, ?, ?, ?)";
+
+  public static String createSaleQuery =
+      "INSERT INTO venda (data_hora, usuario_id) VALUES (CURRENT_TIMESTAMP, ?) RETURNING id";
+
+  public static String createSaleProductQuery =
+      "INSERT INTO venda_produto (venda_id, produto_id, preco, quantidade) "
+          + "VALUES (?, ?, ?, ?)";
 }
