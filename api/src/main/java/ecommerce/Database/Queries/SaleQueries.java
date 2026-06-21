762c6b74878c19a0282e74512ac636216d43ece6
DerikTech <madrinckjr@gmail.com>
2026-06-21 18:26:30 +0000

    ♻️ Refatorei queries de venda removendo duplicações

--- api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
@@ -1,15 +1,14 @@
 package ecommerce.Database.Queries;
 
 public class SaleQueries {
-  public static String insertSaleQuery = "INSERT INTO venda (usuario_id) VALUES (?)";
+
+  public static String insertSaleQuery =
+      "INSERT INTO venda (data_hora, usuario_id) "
+          + "VALUES (CURRENT_TIMESTAMP, ?) "
+          + "RETURNING id";
 
   public static String insertSaleItemQuery =
-      "INSERT INTO venda_produto (venda_id, produto_id, preco, quantidade) VALUES (?, ?, ?, ?)";
-
-  public static String createSaleQuery =
-      "INSERT INTO venda (data_hora, usuario_id) VALUES (CURRENT_TIMESTAMP, ?) RETURNING id";
-
-  public static String createSaleProductQuery =
-      "INSERT INTO venda_produto (venda_id, produto_id, preco, quantidade) "
+      "INSERT INTO venda_produto "
+          + "(venda_id, produto_id, preco, quantidade) "
           + "VALUES (?, ?, ?, ?)";
 }
