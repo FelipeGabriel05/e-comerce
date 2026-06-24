39b218ca303f01eeff3e50f43f998bd60edb55b6
Paulo Victor <112362159+pv-lima@users.noreply.github.com>
2026-06-24 18:43:15 +0000

    feat: add selectSalesByUserIdQuery in queries

--- api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
@@ -5,4 +5,7 @@ public class SaleQueries {
 
   public static String insertSaleItemQuery =
       "INSERT INTO venda_produto (venda_id, produto_id, preco, quantidade) VALUES (?, ?, ?, ?)";
+
+  public static String selectSalesByUserIdQuery =
+      "SELECT id, data_hora, usuario_id FROM venda WHERE usuario_id = ? ORDER BY data_hora DESC";
 }
