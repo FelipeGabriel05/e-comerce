8c51b076f7fba5133c7bea19936ab946124e2bf4
Paulo Victor <112362159+pv-lima@users.noreply.github.com>
2026-06-25 19:50:13 +0000

    feat: add GET /admin/sales in AuthenticationRequiredFilter

--- api/src/main/java/ecommerce/Http/Filter/AuthenticationRequiredFilter.java
+++ api/src/main/java/ecommerce/Http/Filter/AuthenticationRequiredFilter.java
@@ -36,7 +36,8 @@ public class AuthenticationRequiredFilter implements Filter {
           new Route("PUT", "/admin/category/*"),
           new Route("PUT", "/admin/products/*"),
           new Route("POST", "/logout"),
-          new Route("GET", "/sales"));
+          new Route("GET", "/sales")
+          new Route("GET", "/admin/sales"));
 
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {}
