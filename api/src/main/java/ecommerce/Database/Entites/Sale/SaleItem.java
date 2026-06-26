8696e1e760f648dc6cbc4a503ec1d4a59e728826
Paulo Victor <112362159+pv-lima@users.noreply.github.com>
2026-06-26 18:07:55 +0000

    feat: add Oroduct reference to SaleItem entity

--- api/src/main/java/ecommerce/Database/Entites/Sale/SaleItem.java
+++ api/src/main/java/ecommerce/Database/Entites/Sale/SaleItem.java
@@ -2,11 +2,13 @@ package ecommerce.Database.Entites.Sale;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import ecommerce.Database.Entites.Product;
 
 public class SaleItem {
   private int productId;
   private double price;
   private int quantity;
+  private Product product;
 
   public SaleItem(int productId, double price, int quantity) {
     this.productId = productId;
@@ -36,6 +38,14 @@ public class SaleItem {
 
   public void setQuantity(int quantity) {
     this.quantity = quantity;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
   }
 
   public String toJson() {
