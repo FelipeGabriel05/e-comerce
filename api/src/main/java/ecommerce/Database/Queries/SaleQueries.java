dea49ca4776dd5da3ce0dfe6d9ea86e9d6b29e6d
Paulo Victor <112362159+pv-lima@users.noreply.github.com>
2026-06-26 18:36:11 +0000

    refactor: update selectSaleItems query to JOIN with produto table

--- api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
@@ -10,7 +10,7 @@ public class SaleQueries {
       "SELECT id, data_hora, usuario_id FROM venda WHERE usuario_id = ? ORDER BY data_hora DESC";
 
   public static String selectSaleItemsBySaleIdQuery =
-      "SELECT produto_id, preco, quantidade FROM venda_produto WHERE venda_id = ?";
+      "SELECT vp.produto_id, vp.preco, vp.quantidade, p.descricao, p.foto, p.categoria_id FROM venda_produto vp JOIN produto p ON vp.produto_id = p.id WHERE vp.venda_id = ?";
 
   public static String selectAllSalesQuery =
       "SELECT id, data_hora, usuario_id FROM venda ORDER BY data_hora DESC";
