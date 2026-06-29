9e6c5f1927da84c72c4c152ef06886983708a7b6
DerikTech <madrinckjr@gmail.com>
2026-06-29 22:24:42 +0000

    ✨ Adiciona endpoint para exclusão de compras

---
+++ api/src/main/java/ecommerce/Http/Controller/AdminSaleController.java
@@ -0,0 +1,53 @@
+package ecommerce.Http.Controller;
+
+import ecommerce.Exceptions.NotFoundException;
+import ecommerce.Http.IO.PathVariableExtractor;
+import ecommerce.Http.IO.Responses.JsonResponse;
+import ecommerce.UseCases.DeleteSaleUseCase;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/sales/*")
+public class AdminSaleController extends HttpServlet {
+
+  @Override
+  protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    response.setContentType("application/json");
+
+    try {
+      int id = PathVariableExtractor.extractIntPathVariable(request, "/admin/sales/:id", "id");
+
+      DeleteSaleUseCase useCase = new DeleteSaleUseCase();
+
+      useCase.execute(id);
+
+      JsonResponse jsonRes =
+          new JsonResponse(HttpServletResponse.SC_OK, "Sale deleted successfully");
+
+      response.setStatus(jsonRes.getStatus());
+      response.getWriter().write(jsonRes.toJson());
+
+    } catch (NotFoundException e) {
+
+      JsonResponse jsonRes = new JsonResponse(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
+
+      response.setStatus(jsonRes.getStatus());
+      response.getWriter().write(jsonRes.toJson());
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+
+      JsonResponse jsonRes =
+          new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+
+      response.setStatus(jsonRes.getStatus());
+      response.getWriter().write(jsonRes.toJson());
+    }
+  }
+}
