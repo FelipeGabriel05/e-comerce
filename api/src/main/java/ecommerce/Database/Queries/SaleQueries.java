a9f2381fd8f75721211999f6028b9b59ad91ac3b
DerikTech <madrinckjr@gmail.com>
2026-06-29 22:24:05 +0000

    ✨ Adiciona consultas para exclusão de compras

--- api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
+++ api/src/main/java/ecommerce/Database/Queries/SaleQueries.java
@@ -5,4 +5,9 @@ public class SaleQueries {
 
   public static String insertSaleItemQuery =
       "INSERT INTO venda_produto (venda_id, produto_id, preco, quantidade) VALUES (?, ?, ?, ?)";
+
+  public static String deleteSaleItemsBySaleIdQuery =
+      "DELETE FROM venda_produto WHERE venda_id = ?";
+
+  public static String deleteSaleByIdQuery = "DELETE FROM venda WHERE id = ?";
 }
